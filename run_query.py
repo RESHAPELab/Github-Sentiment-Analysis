f53f6303047d79aec9a0cef2886954b9ab7adfeb
Champ Foronda <champ.foronda@gmail.com>
2019-09-25 10:50:09 -0700

    Updated requirements.txt and fixed formatting for run_query.py

--- run_query.py
+++ run_query.py
@@ -12,88 +12,74 @@ from config import GITHUB_AUTHORIZATION_KEY
 headers = {"Authorization": GITHUB_AUTHORIZATION_KEY}
 
 def setup_query(owner = "", name = "", pull_request_number = 1, comment_range = 10):
-  query = f'''
-  query {{
-  repository(owner: {owner}, name: {name}) {{
-    pullRequest: issueOrPullRequest(number: {pull_request_number}) {{
-      __typename
-      ... on PullRequest {{
-        title
-        number
-        closed
-        author {{
-            login 
-        }}
-        bodyText
-        comments(first: {comment_range}) {{
-          edges {{
-            node {{
-              author {{
-                login
-              }}
-              bodyText
-            }}
-          }}
-        }}
-        reviewThreads(first: 100) {{
-          edges {{
-            node {{
-              comments(first: {comment_range}) {{
-                nodes {{
-                  author {{
-                    login
-                  }}
-                  bodyText
-                  authorAssociation
+    query = f'''
+    query {{
+        repository(owner: {owner}, name: {name}) {{
+            pullRequest: issueOrPullRequest(number: {pull_request_number}) {{
+            __typename
+            ... on PullRequest {{
+                title
+                number
+                closed
+                author {{
+                    login 
                 }}
-              }}
+                bodyText
+                comments(first: {comment_range}) {{
+                edges {{
+                    node {{
+                    author {{
+                        login
+                    }}
+                    bodyText
+                    }}
+                }}
+                }}
+                reviewThreads(first: 100) {{
+                edges {{
+                    node {{
+                    comments(first: {comment_range}) {{
+                        nodes {{
+                        author {{
+                            login
+                        }}
+                        bodyText
+                        authorAssociation
+                        }}
+                    }}
+                    }}
+                }}
+                }}
             }}
-          }}
+            }}
         }}
-      }}
-    }}
-  }}
-  }}'''
-  return query
+        }}'''
+    return query
 
 # Funtion that uses requests.post to make the API call
 def run_query(query):
-  request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
-  if request.status_code == 200:
-      return request.json()
-  else:
-        raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
 
 def get_comments_from_review_threads(query_data):
-  review_nodes = query_data['data']['repository']['pullRequest']['reviewThreads']['edges']
-  list_of_comments = dict()
-  for review_node in review_nodes:
-    for comment in review_node['node']['comments']['nodes']:
-      list_of_comments.update({comment['author']['login'] : comment['bodyText']})
+    review_nodes = query_data['data']['repository']['pullRequest']['reviewThreads']['edges']
+    dict_of_comments = dict()
+    for review_node in review_nodes:
+        for comment in review_node['node']['comments']['nodes']:
+        dict_of_comments.update({comment['author']['login'] : comment['bodyText']})
 
-  return list_of_comments
+    return dict_of_comments
 
 def get_comments_from_pull_request(query_data):
-  comment_edges = query_data['data']['repository']['pullRequest']['comments']['edges']
-  list_of_comments = dict()
-  for edge in comment_edges:
-    list_of_comments.update({edge['node']['author']['login'] : edge['node']['bodyText']})
+    comment_edges = query_data['data']['repository']['pullRequest']['comments']['edges']
+    dict_of_comments = dict()
+    for edge in comment_edges:
+        dict_of_comments.update({edge['node']['author']['login'] : edge['node']['bodyText']})
 
-  return list_of_comments
-
-def write_pull_request_comments_to_csv(list_of_comments=[]):
-  with open('pull_request_comments.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    for comment in list_of_comments:
-      writer.writerow([comment])
-    csv_file.close()
-
-def write_review_comments_to_csv(list_of_comments=[]):
-  with open('review_thread_comments.csv', 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file, delimiter=',')
-    for comment in list_of_comments:
-      writer.writerow([comment])
-    csv_file.close()
+    return dict_of_comments
 
 owner_name = "astropy"
 repo_name = "astropy"
