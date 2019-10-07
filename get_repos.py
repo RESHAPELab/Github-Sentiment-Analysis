b3a830eb1bb236f0537ff63c71d2e6dfd26d4f0e
Josh <heyjoshkruse@gmail.com>
2019-10-07 15:27:14 -0700

    Succesfully pages through repositories and adds each one to a mongodb with information needed

--- get_repos.py
+++ get_repos.py
@@ -12,14 +12,14 @@ import datetime
 from config import GITHUB_AUTHORIZATION_KEY, MONGO_USER, MONGO_PASSWORD
 
 # Variables
-headers = {"Authorization": "GITHUB_AUTHORIZATION_KEY"}
-owner_name = "astropy"
-repo_name = "astropy"
-number_of_pull_requests = 20
-comment_range = 100
+headers = {"Authorization": "token "}
 mongo_client_string = "mongodb+srv://" + MONGO_USER + ":" + MONGO_PASSWORD + "@sentiment-analysis-8snlg.mongodb.net/test?retryWrites=true&w=majority"
-database_name = repo_name + "_database"
+database_name = "repository_database"
 collection_name = "comments"
+min_stars = 1000
+max_stars = 10000
+last_activity = 90 # within the last __ days
+created = 364 * 4 # within the last __ days
 
 # Defines the query to run
 def setup_query( query_string, end_cursor ):
@@ -30,7 +30,7 @@ def setup_query( query_string, end_cursor ):
         remaining
         resetAt
        }}
-       search(query: "{query_string}", type: REPOSITORY, first:2 {end_cursor}) {{
+       search(query: "{query_string}", type: REPOSITORY, first:10 {end_cursor}) {{
            pageInfo {{
                endCursor
                hasNextPage
@@ -124,39 +124,50 @@ def query_filter( min_stars, max_stars, last_activity, created ):
     return f'is:public archived:false fork:false stars:{stars} pushed:20{date_last_act:%y-%m-%d}..* created:20{date_created:%y-%m-%d}..*'
 
 # Runs the query and iterates through all pages of repositories
-def find_repos( query_string, database, db_collection ):
+def find_repos( query_string, db_collection ):
 
     end_cursor = ""
     end_cursor_string = ""
     hasNextPage = True
     index = 0
     
-    while( hasNextPage and index <= 4 ):
+    while( hasNextPage and index <= 1 ):
         query = setup_query( query_string, end_cursor_string )
         result = run_query( query )
         print(json.dumps(result, indent=2))
-        index += 1
-        
+
+        repo_checker( result, db_collection )
+
+        # if there is a next page, update the endcursor string and continue loop
         if( result["data"]["search"]["pageInfo"]["hasNextPage"] ):
             end_cursor = result["data"]["search"]["pageInfo"]["endCursor"]
             end_cursor_string = f', after:"{end_cursor}"'
         else:
             hasNextPage = False
 
+        index += 1
+
 # Iterates through all repositories found on each page
 # saves valid repositories into a database
-def repo_checker( query_data, database, db_collection ):
-    return false #TODO
+def repo_checker( query_data, db_collection ):
+
+    repository_nodes = query_data["data"]["search"]["nodes"]
+    dict_of_repositories = {"repository" : []}
+    for node in repository_nodes:
+        dict_of_repositories["repository"].append( {"name" : node["name"],
+                                                    "owner" : node["owner"]["login"],
+                                                    "contributers" : node["contributors"]["totalCount"],
+                                                    "stars" : node["stargazers"]["totalCount"],
+                                                    "forks" : node["forks"],
+                                                    "commits" : node["commits"]["target"]["history"]["totalCount"],
+                                                    "pullRequests" : node["pullRequests"]["totalCount"]} )
+
+    db_collection.insert_one( dict_of_repositories )
 
 # checks that a repository is valid based off of parameters
 # returns a boolean
 def is_repo_valid( ):
     return false #TODO
-
-min_stars = 1000
-max_stars = 10000
-last_activity = 90 # within the last __ days
-created = 364 * 4 # within the last __ days
 
 # create the query filter and setup the query string
 query_string = query_filter( min_stars, max_stars, last_activity, created )
@@ -168,7 +179,7 @@ database = client[ database_name ]
 db_collection = database[ collection_name ]
 
 # run the query
-find_repos( query_string, database, db_collection )
+find_repos( query_string, db_collection )
 print( "did i get here" )
 
 # Adds comments to a MongoDB
