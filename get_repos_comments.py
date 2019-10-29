089bedde25a33b1b84d669663d4ae6a42be6d23f
Josh <heyjoshkruse@gmail.com>
2019-10-28 17:11:12 -0700

    Added a new script that runs through repos with given parameters, and for each valid repo, will collect its pull request comments and save them to a database collection

---
+++ get_repos_comments.py
@@ -0,0 +1,308 @@
+'''
+Filename: get_repos.py
+Author(s): Joshua Kruse and Champ Foronda
+Description: Script that runs a query on repositories on GitHub and writes them into a MongoDatabase
+'''
+
+# imports
+import requests
+import json
+import csv
+import pymongo
+import time
+import datetime
+from config import GITHUB_AUTHORIZATION_KEY, MONGO_USER, MONGO_PASSWORD
+
+# Variables
+headers = {"Authorization": "token 5082a27709b387074fbcc97d778b9943724b6c58"}
+mongo_client_string = "mongodb+srv://" + MONGO_USER + ":" + MONGO_PASSWORD + "@sentiment-analysis-8snlg.mongodb.net/test?retryWrites=true&w=majority"
+min_stars = 0
+max_stars = 10
+last_activity = 90 # within the last __ days
+created = 364 * 4 # withinthe last __ days
+total_pull_num = 0 # amount of pull requests a repository needs
+number_of_pull_requests = 10
+repo_database_name = "repositories"
+repo_collection_name = "collect_mnst" + str(min_stars) + "_mxst" + str(max_stars) + "_lsact" + str(last_activity) + "_crtd" + str(created) + "_nmpll" + str(total_pull_num)
+pull_database_name = "comments"
+pull_collection_name = "default_collection"
+
+
+'''
+--- Workflow ---
+    I. Create query string and build query
+    II. run query, iterating through all pages of repos:
+        i. iterate through all repositories on each page:
+            I. add specific repository to database if it matches specific parameters
+    III. Pull comments from each repository in repo_database and save to a new database
+'''
+
+# Runs the query and iterates through all pages of repositories
+def find_repos( ):
+
+    # create the query filter and setup the query string
+    query_string = query_filter( min_stars, max_stars, last_activity, created )
+
+    # Establishing connection to mongoClient
+    client = pymongo.MongoClient( mongo_client_string )
+    repo_database = client[ repo_database_name ]
+    db_repo_collection = repo_database[ repo_collection_name ]
+
+    end_cursor = ""
+    end_cursor_string = ""
+    hasNextPage = True
+    index = 0
+    
+    while( hasNextPage ):
+        query = setup_repo_query( query_string, end_cursor_string )
+        result = run_query( query )
+
+        repo_checker( result, db_repo_collection, total_pull_num, client, db_repo_collection )
+
+        # if there is a next page, update the endcursor string and continue loop
+        if( result["data"]["search"]["pageInfo"]["hasNextPage"] ):
+            end_cursor = result["data"]["search"]["pageInfo"]["endCursor"]
+            end_cursor_string = f', after:"{end_cursor}"'
+        else:
+            hasNextPage = False
+
+        index += 1
+        time.sleep(1)
+
+# Builds the query filter string compatible to github
+def query_filter( min_stars, max_stars, last_activity, created ):
+    date_last_act = datetime.datetime.now() - datetime.timedelta( days=last_activity )
+    date_created = datetime.datetime.now() - datetime.timedelta( days=created )
+    stars = f'{min_stars}..{max_stars}'
+
+    return f'is:public archived:false fork:false stars:{stars} pushed:20{date_last_act:%y-%m-%d}..* created:20{date_created:%y-%m-%d}..*'
+
+# Funtion that uses requests.post to make the API call
+def run_query(query):
+    request = requests.post('https://api.github.com/graphql', json={'query': query}, headers=headers)
+    if request.status_code == 200:
+        return request.json()
+    else:
+            raise Exception("Query failed to run by returning code of {}. {}".format(request.status_code, query))
+
+# Iterates through all repositories found on each page
+# saves valid repositories into a database
+def repo_checker( query_data, db_collection, total_pull_num, client, db_repo_collection ):
+
+    repository_nodes = query_data["data"]["search"]["nodes"]
+    dict_of_repositories = {"repository" : []}
+    list_of_repositories = []
+
+    index = 0
+    for node in repository_nodes:
+        if( is_repo_valid( node, total_pull_num ) ):
+
+            repo_owner = node['owner']['login']
+            repo_name = node['name']
+
+            # Save comments from each valid repo to a database
+            get_comments( repo_owner, repo_name, client )
+            
+            list_of_repositories.append( {"name" : node["name"],
+                                                        "owner" : node["owner"]["login"],
+                                                        "contributers" : node["contributors"]["totalCount"],
+                                                        "stars" : node["stargazers"]["totalCount"],
+                                                        "forks" : node["forks"],
+                                                        "commits" : node["commits"]["target"]["history"]["totalCount"],
+                                                        "pullRequests" : node["pullRequests"]["totalCount"]} )
+            print( "Repository: " + str(index) )
+            index += 1
+
+    db_repo_collection.insert_many( list_of_repositories )
+
+# checks that a repository is valid
+# a repo is valid if it has more pull requests than the param
+# TODO: more requirments?
+# returns a boolean
+def is_repo_valid( node, total_pull_num ):
+    if( node["pullRequests"]["totalCount"] > total_pull_num ):
+        return True
+    return False
+
+# Saves all comments on pull requests in a given repository to a database
+# Takes in only the owner of a repository and the name
+def get_comments( repo_owner, repo_name, client ):
+
+    pull_database = client[ pull_database_name ]
+    db_pull_collection = pull_database[ repo_owner + "__" + repo_name ]
+
+    # loops through every pull request made on a repository
+    for pull_request_index in range( 1, number_of_pull_requests + 1 ):
+        
+        query = setup_pull_query( repo_owner, repo_name, 1, 100 )
+        query_data = run_query( query )
+
+        # Gets the pull request comments
+        list_of_comments = []
+        try:
+            comment_edges = query_data['data']['repository']['pullRequest']['comments']['edges']
+
+            index = 0
+            for edge in comment_edges:
+                list_of_comments.append( {'author' : edge['node']['author']['login'],
+                                              'bodyText' : edge['node']['bodyText'] } )
+
+                print( "Comment: " + str( index ) )
+
+                index += 1
+                
+        except KeyError:
+            print("Is an Issue")
+
+        if( list_of_comments ):
+            db_pull_collection.insert_many( list_of_comments )
+
+        # Gets the pull request review comments
+        list_of_review_comments = []
+        try:
+            review_nodes = query_data['data']['repository']['pullRequest']['reviewThreads']['edges']
+
+            index = 0
+            for review_node in review_nodes:
+                for comment in review_node['node']['comments']['nodes']:
+                    list_of_review_comments.append( {'author' : comment['author']['login'],
+                                                     'bodyText' : comment['bodyText'] } )
+                    print( "Comment: " + str( index ) )
+                    index += 1
+        except KeyError:
+            print("Is an Issue")
+                        
+        if( list_of_review_comments ):
+            db_pull_collection.insert_many( list_of_review_comments )
+
+# Defines the query to run
+def setup_repo_query( query_string, end_cursor ):
+    query = f'''
+    query {{
+       rateLimit{{
+        cost
+        remaining
+        resetAt
+       }}
+       search(query: "{query_string}", type: REPOSITORY, first:50 {end_cursor}) {{
+           pageInfo {{
+               endCursor
+               hasNextPage
+            }}
+            repositoryCount
+            nodes {{
+                ... on Repository {{
+                    owner {{
+                        login
+                    }}
+                    name
+                    createdAt
+                    pushedAt
+                    isMirror
+                    diskUsage
+                    primaryLanguage {{
+                        name
+                    }}
+                    languages {{
+                        totalCount
+                    }}
+                    contributors: mentionableUsers {{
+                        totalCount
+                    }}
+                    watchers {{
+                        totalCount
+                    }}
+                    stargazers {{
+                        totalCount
+                    }}
+                    forks: forkCount
+                    issues {{
+                        totalCount
+                    }}
+                    commits: defaultBranchRef {{
+                        target {{
+                            ... on Commit {{
+                                history {{
+                                    totalCount
+                                }}
+                            }}
+                        }}
+                    }}
+                    pullRequests(last:10){{
+                        totalCount
+                        nodes {{
+                            createdAt
+                        }}
+                    }}
+                    branches: refs(refPrefix: "refs/heads/") {{
+                        totalCount
+                    }}
+                    tags: refs(refPrefix: "refs/tags/") {{
+                        totalCount
+                    }}
+                    releases {{
+                        totalCount
+                    }}
+                    description
+                }}
+            }}
+        }}
+    }}'''
+    return query
+
+# Defines the query to run
+def setup_pull_query(owner = "", name = "", pull_request_number = 1, comment_range = 10):
+    query = f'''
+    query {{
+        repository(owner: "{owner}", name: "{name}") {{
+            pullRequest: issueOrPullRequest(number: {pull_request_number}) {{
+            __typename
+            ... on PullRequest {{
+                title
+                number
+                closed
+                author {{
+                    login
+                }}
+                bodyText
+                comments(first: {comment_range}) {{
+                edges {{
+                    node {{
+                    author {{
+                        login
+                    }}
+                    bodyText
+                    }}
+                }}
+                }}
+                reviewThreads(first: 100) {{
+                edges {{
+                    node {{
+                    comments(first: {comment_range}) {{
+                        nodes {{
+                        author {{
+                            login
+                        }}
+                        bodyText
+                        authorAssociation
+                        }}
+                    }}
+                    }}
+                }}
+                }}
+            }}
+            }}
+        }}
+        }}'''
+    return query
+
+# run the query
+find_repos( )
+
+# Adds comments to a MongoDB
+# client = pymongo.MongoClient("mongodb+srv://jek248:SentimentAnalysis@sentiment-analysis-8snlg.mongodb.net/test?retryWrites=true&w=majority")
+# db = client['testing_db']
+# db_collection = db['pull_request_comments']
+# db_collection.insert_one( list_of_pull_request_comments )
+# db_collection.insert_one( list_of_review_thread_comments )
+# client.close()
