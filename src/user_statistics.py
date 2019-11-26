d882c4d4a201cb4adb718bada3a0acc522170af9
Champ Foronda <champ.foronda@gmail.com>
2019-11-25 23:14:13 -0700

    Made it so that the csv files writes to the resources folder at the project root directory

--- src/user_statistics.py
+++ src/user_statistics.py
@@ -1,33 +1,35 @@
+from Config import MONGO_CLIENT_STRING
 from pymongo import MongoClient
 from collections import defaultdict
 import pprint
 import csv
+import os
 
-MONGO_CLIENT_STRING = "mongodb+srv://jek248:SentimentAnalysis@sentiment-analysis-8snlg.mongodb.net/test?retryWrites=true&w=majority"
-
-client = MongoClient( MONGO_CLIENT_STRING )
-
-db = client[ 'AUTHOR_INFO_BY_REPO' ]
+client = MongoClient(MONGO_CLIENT_STRING)
+db = client['AUTHOR_INFO_BY_REPO']
 collections = db.list_collection_names()
 
-print( db[ collections[0] ].find_one(sort=[('total_for_repo', -1)]) )
-print(db[ collections[0] ].count_documents( {'total_for_repo' : 1} ))
+# print(db[collections[0]].find_one(sort=[('total_for_repo', -1)]))
+# print(db[collections[0]].count_documents( {'total_for_repo' : 1}))
 
 count = defaultdict(int)
 
 for collect in collections:
     print(f"[WORKING] Gathering documents from: {collect}")
-    for index in range( db[collect].find_one(sort=[('total_for_repo', -1)])['total_for_repo'] ):
-        amount = db[collect].count_documents({'total_for_repo': index+1})
+    for index in range(db[collect].find_one(sort=[('total_for_repo', -1)])['total_for_repo']):
+        amount = db[collect].count_documents({'total_for_repo': index + 1})
         if amount > 0:
             print(f"[WORKING] Gathering count from: {collect} where index: {index}")
-            count[ index+1 ] += amount
-    print( count )
+            count[index + 1] += amount
+    print(count)
 
-#writing
-with open('results.csv', 'w') as f:
-    writer = csv.DictWriter(f, count.keys())    
+
+save_path = os.path.join(os.getcwd()[:-3], "resources/results.csv")
+
+# Writes results.csv into a resources folder in source directory
+with open(save_path, 'w') as csv_file:
+    writer = csv.DictWriter(csv_file, count.keys())    
     writer.writeheader()
-    writer.writerow( count )
+    writer.writerow(count)
 
-print( count )
+print(count)
